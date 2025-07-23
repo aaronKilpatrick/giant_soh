b354acae1c9be6664bfb867e774e702713147191
Aaron Kilpatrick <kilpatrick.aaron92@gmail.com>
2025-07-23 13:08:11 +1000

    Add progress bar

--- giant_scrapper.py
+++ giant_scrapper.py
@@ -2,17 +2,24 @@ from selenium import webdriver
 from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as ec
 import csv
+import time
+import sys
 
 def get_default_chrome_options():
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless=new")
+    # options.add_argument("--headless=new")
     options.add_argument("--start-maximized")
     options.add_experimental_option("detach", True)
     # options.add_argument("--log-level=3")
     return options
+
+def get_driver_path():
+    if sys.platform == "linux":
+        return './chromedriver_linx64'
     
 def close_modal_by_selector(driver, selector):
     try:
@@ -39,7 +46,12 @@ def close_popups_on_load(driver):
 
 def get_product_name(driver):
     WebDriverWait(driver, 5).until(ec.title_contains('Australia'))
-    return driver.title.split('|')[0].strip()
+    page_title = driver.title.split('|')
+    page_title = [title.strip() for title in page_title]
+    
+    if "The Total Race Bike" in page_title or "Ultra-light Endurance Road Bike" in page_title:
+        return page_title[1].strip()
+    return page_title[0].strip()
 
 def get_element_after_visible(driver, time_wait, selector):
     return WebDriverWait(driver, time_wait).until(ec.visibility_of_element_located((By.CSS_SELECTOR, selector)))
@@ -59,7 +71,6 @@ def get_model_stock_levels(driver, soh_list):
         size = s.text
         sku = get_sku(driver)
         stock_level = get_sku_stock_level(driver)
-        print(sku + size + product_name)
         soh_list.append([sku, product_name, size, stock_level])
     return soh_list
 
@@ -77,34 +88,53 @@ def write_to_csv(soh_list):
         csv_writer.writerow(header)
         csv_writer.writerows(soh_list)
 
+def progress_indicator(list_length, list_index):
+    if list_length == 0: return
+    
+    list_index = list_index + 1
+    
+    if list_index == list_length:
+        print("\rProgress: 100%", end="", flush=True)
+        time.sleep(0.5)
+        print("\rProgress: Complete", flush=True)
+        return
+    
+    progress = int(list_index / list_length * 100)
+    print(f"\rProgress: {progress}%", end="", flush=True)
+    
 def main():
-    soh_list = []
     page_visted = {
         "liv": 0,
         "giant": 0,
     }
-    product_urls = get_url_list()
-    options = get_default_chrome_options()
-    driver = webdriver.Chrome(options=options)
     
-    for url in product_urls:
-        # options = get_default_chrome_options()
-        # driver = webdriver.Chrome(options=options)
+    soh_list = []
+    product_urls = get_url_list()
+    product_urls_len = len(product_urls)
+    
+    options = get_default_chrome_options()
+    service = Service(executable_path=get_driver_path())
+    driver = webdriver.Chrome(service=service, options=options)
+    
+    for i, url in enumerate(product_urls):
         driver.get(url)
+        
         if domain_first_visit(page_visted, url):
             close_popups_on_load(driver)
-            
-        print(url)
+           
         # wait for the product configurator to be ready
         get_element_after_visible(driver, 10, "#product-configurator")
-        colours = driver.find_elements(By.CSS_SELECTOR, ".colors label")
-        if len(colours) > 0:
-            for colour in colours:
-                colour.click()
+        colour_btns = driver.find_elements(By.CSS_SELECTOR, ".colors label")
+        
+        if len(colour_btns) > 0:
+            for colour_btn in colour_btns:
+                colour_btn.click()
                 soh_list = get_model_stock_levels(driver, soh_list)
         else:    
             soh_list = get_model_stock_levels(driver, soh_list)
-    
+        
+        progress_indicator(product_urls_len, i)
+        
     driver.close()  
     
     write_to_csv(soh_list)
