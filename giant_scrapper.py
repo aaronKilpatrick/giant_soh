bb500a1548ded244a38755cc735eac332ae964d0
Aaron Kilpatrick <kilpatrick.aaron92@gmail.com>
2025-07-23 15:39:27 +1000

    removed size from CSV due to inconsistent sizing labels on source site

--- giant_scrapper.py
+++ giant_scrapper.py
@@ -11,7 +11,7 @@ import sys
 def get_default_chrome_options():
     options = webdriver.ChromeOptions()
     options.add_argument("--no-sandbox")
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--start-maximized")
     options.add_experimental_option("detach", True)
     # options.add_argument("--log-level=3")
@@ -23,7 +23,7 @@ def get_driver_path():
     
 def close_modal_by_selector(driver, selector):
     try:
-        modal_close_button = WebDriverWait(driver, 2).until(ec.element_to_be_clickable((By.CSS_SELECTOR, selector)))
+        modal_close_button = WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.CSS_SELECTOR, selector)))
         modal_close_button.click()
     except TimeoutException:
         print("Modal could not be closed")
@@ -49,7 +49,7 @@ def get_product_name(driver):
     page_title = driver.title.split('|')
     page_title = [title.strip() for title in page_title]
     
-    if "The Total Race Bike" in page_title or "Ultra-light Endurance Road Bike" in page_title:
+    if "The Total Race Bike" in page_title or "Ultra-light Endurance Road Bike" in page_title or "Climb higher" in page_title:
         return page_title[1].strip()
     return page_title[0].strip()
 
@@ -57,10 +57,10 @@ def get_element_after_visible(driver, time_wait, selector):
     return WebDriverWait(driver, time_wait).until(ec.visibility_of_element_located((By.CSS_SELECTOR, selector)))
 
 def get_sku(driver):
-    return get_element_after_visible(driver, 2,"#selected-partnumber span").text
+    return get_element_after_visible(driver, 10,"#selected-partnumber span").text
 
 def get_sku_stock_level(driver):
-    stock_level = get_element_after_visible(driver, 2,".stock-info-wrapper .stockinfo")
+    stock_level = get_element_after_visible(driver, 10,".stock-info-wrapper .stockinfo")
     return "0" if stock_level.text == 'Contact Store' else "1"
 
 def get_model_stock_levels(driver, soh_list):
@@ -68,10 +68,9 @@ def get_model_stock_levels(driver, soh_list):
     product_name = get_product_name(driver)
     for s in size_buttons:
         s.click()
-        size = s.text
         sku = get_sku(driver)
         stock_level = get_sku_stock_level(driver)
-        soh_list.append([sku, product_name, size, stock_level])
+        soh_list.append([sku, product_name, stock_level])
     return soh_list
 
 def get_url_list():
@@ -80,7 +79,7 @@ def get_url_list():
     return url_list
 
 def write_to_csv(soh_list):
-    header = ["SKU", "PRODUCT_DESCRIPTION", "SIZE", "STOCK_LEVEL"]
+    header = ["SKU", "PRODUCT_DESCRIPTION", "STOCK_LEVEL"]
     
     with open("giant_liv_datafeed.csv", "w", newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
@@ -103,6 +102,7 @@ def progress_indicator(list_length, list_index):
     print(f"\rProgress: {progress}%", end="", flush=True)
     
 def main():
+    start = time.time()
     page_visted = {
         "liv": 0,
         "giant": 0,
@@ -118,7 +118,7 @@ def main():
     
     for i, url in enumerate(product_urls):
         driver.get(url)
-        
+        print(url)
         if domain_first_visit(page_visted, url):
             close_popups_on_load(driver)
            
@@ -133,11 +133,11 @@ def main():
         else:    
             soh_list = get_model_stock_levels(driver, soh_list)
         
-        progress_indicator(product_urls_len, i)
+        # progress_indicator(product_urls_len, i)
         
     driver.close()  
     
     write_to_csv(soh_list)
-
+    print(time.time() - start)
 if __name__ == "__main__":
     main()
